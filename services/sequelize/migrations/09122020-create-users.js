a19d590364aabbc3b76241221e10e8d83b15c082
karl.kaspar <>
2020-12-09 20:40:02 +0200

    Added db migration capability

---
+++ services/sequelize/migrations/09122020-create-users.js
@@ -0,0 +1,29 @@
+module.exports.up = (queryInterface, DataTypes) => {
+  // create users table
+  return queryInterface.createTable("users", {
+    id: {
+      allowNull: false,
+      autoincrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER.UNSIGNED
+    },
+    firstName: {
+      allowNull: false,
+      type: DataTypes.STRING(50)
+    },
+    lastName: {
+      allowNull: false,
+      type: DataTypes.STRING(50)
+    },
+    email: {
+      allowNull: false,
+      type: DataTypes.STRING(50)
+    },
+    password: {
+      allowNull: false,
+      type: DataTypes.STRING(50)
+    }
+  });
+};
+
+module.exports.down = (queryInterface) => queryInterface.dropTable("users");
